256b976224ce073f2ed9680653c3fdbacd97119b
Aamir Khan <oneaamirkhan@icloud.com>
2023-11-13 15:16:34 +0530

    Initial Commit

---
+++ Barcode_Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode_Scanner
+//
+//  Created by عامر خان on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
