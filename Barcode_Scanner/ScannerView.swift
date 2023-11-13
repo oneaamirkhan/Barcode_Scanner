3f221e5e629f38f94d46551e3735c0c368767741
Aamir Khan <oneaamirkhan@icloud.com>
2023-11-13 17:40:27 +0530

    UIKit functionality added and ScannerView completed

---
+++ Barcode_Scanner/ScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScannerView.swift
+//  Barcode_Scanner
+//
+//  Created by عامر خان on 13/11/23.
+//
+
+import SwiftUI
+
+struct ScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScannerView()
+}
